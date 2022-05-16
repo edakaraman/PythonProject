10d0976c7bf4640c4eab46b8a4dbcd6520ea36b5
Eda Karaman <winx_bloom_eda@hotmail.com>
2022-05-16 22:28:28 +0300

    ikinci hafta kodları eklendi

--- RakamTanima.py
+++ RakamTanima.py
@@ -26,3 +26,36 @@ model.add(tf.keras.layers.Dense(256, activation='relu'))
 model.add(tf.keras.layers.Dropout(0.5))
 model.add(tf.keras.layers.Dense(10, activation='softmax'))
 model.summary()
+
+model.compile(
+	           loss='sparse_categorical_crossentropy',
+               optimizer='adam',
+               metrics=['accuracy']
+             )
+
+x_train = x_train.reshape(-1,28, 28,1) #Reshape for CNN 
+x_test = x_test.reshape(-1,28, 28, 1)
+model_log=model.fit(
+	                  x_train,
+	                  y_train,
+                      batch_size=60,
+          		      epochs=10,
+          			  verbose=1,
+          			  validation_split=.3
+          			)
+
+# Evaluate the model on test set
+score = model.evaluate(x_test, y_test, verbose=0)
+print('\n', 'Test accuracy:', score[1])
+
+predictions = model.predict(x_test)
+predictions[0]
+np.argmax(predictions[0])
+x_test = x_test.reshape(-1,28, 28)
+x_test.shape
+
+# Show one of the images from the test dataset
+plt.xticks([])
+plt.yticks([])
+plt.xlabel([y_train[0]])
+plt.imshow(x_test[0],cmap=plt.cm.binary)
